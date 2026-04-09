8fb171a76a8446ee3c110cd44ec4abec4ec7e512
Alaguvel A <alaguvel431@gmail.com>
2026-04-09 21:16:58 +0530

    loops.py
    Add loops practice with for and while

---
+++ loops.py
@@ -0,0 +1,15 @@
+# Loops 
+print("=" * 35)
+print("For Loop - Multiplication Table")
+print("=" * 35)
+for i in range(1, 11):
+    print("5 x " + str(i) + " = " + str(5 * i))
+
+print("=" * 35)
+print("While Loop - Countdown")
+print("=" * 35)
+count = 10
+while count >= 1:
+    print("Count : " + str(count))
+    count = count - 1
+print("Done!")
